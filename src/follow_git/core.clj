fd1a508309b8e4901f60636f0724466a6dc3b633
Stefan Dorunga <stefandorunga@gmail.com>
2015-09-10 11:12:56 +0100

    Refactor into seperate functions

--- src/follow_git/core.clj
+++ src/follow_git/core.clj
@@ -13,16 +13,16 @@
   are not currently being followed"
   [& [user password repo]]
   (swap! auth assoc :auth (str user ":" password))
-  (println (:auth @auth) "-" repo)
   (follow-unfollowed-users user repo))
 
-(defn all-entries
-  [git-function search-term]
-  (letfn [(entries-for-page [page] (git-function search-term (conj {:page page} @auth)))]
-    (reduce  (fn [coll curr]
-               (let [entries (entries-for-page curr)]
-                 (if (or (= (count entries) 0)
-                         (map? entries))
+(defn- entries-for-page [git-function search-term page] (git-function search-term (conj {:page page} @auth)))
+
+(defn all-entries [git-function search-term]
+  (future
+    (reduce (fn [coll curr]
+              (let [entries (entries-for-page git-function search-term curr)]
+                (if (map? entries) (throw (Exception. (str "Github Error: " entries))))
+                (if (= (count entries) 0)
                   (reduced coll)
                   (flatten (conj coll entries)))))
              []
@@ -30,15 +30,21 @@
 
 (defn users-following [user]
   (println "Finding all followers")
-  (future (all-entries tentacles.users/following user)))
+  (all-entries tentacles.users/following user))
 
 (defn members-of-org [org]
-  (println "finding all members")
-  (future (all-entries tentacles.orgs/members org)))
+  (println "Finding all members")
+  (all-entries tentacles.orgs/members org))
+
+(defn- set-of-logins [users]
+  (into #{}
+        (map :login users)))
+
+(defn- follow [user]
+  (println "Following user" user)
+  (future (tentacles.users/follow user @auth)))
 
 (defn follow-unfollowed-users [user repo]
-  (letfn [(set-of-logins [users] (into #{} (map :login users)))
-          (follow [user] (println "Following user" user) (future (tentacles.users/follow user @auth)))]
     (let [followed (users-following user)
           org-members (members-of-org repo)
           unfollowed-users (s/difference (set-of-logins @org-members) (set-of-logins @followed))]
@@ -46,4 +52,4 @@
                   (map follow)
                   (map deref)))
       (println "Newly followed users: " (count unfollowed-users))
-      (println "DONE"))))
+      (println "DONE")))
